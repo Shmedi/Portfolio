562721502ddb3edb1e78d7359fc4b0db5143a421
Shmedi <eduard.tupy@hotmail.com>
2020-08-25 15:01:26 -0400

    Organized partials into folders, added JS for section scroll on nav click

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const shmedi = {};
+$(function () {
+  $(document).on("click", 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($(this).attr("href")).offset().top,
+        },
+        300,
+        "linear"
+      );
+  });
+});
