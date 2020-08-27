17f795b2d1bc925d2c7d6ef3dc99bb5a8d7fe969
Shmedi <eduard.tupy@hotmail.com>
2020-08-27 11:36:02 -0400

    Modified php return values, turned auto complete off for inputs

--- formHandler.php
+++ formHandler.php
@@ -3,21 +3,19 @@
   $visitor_email = $_POST["email"];
   $message = $_POST["message"];
 
-  $email_from = $visitor_email;
-
   $email_subject = "New Form Submission";
 
   $email_body = 
   
-  "User Name: $name.\n".
-  "User Email: $visitor_email.\n".
+  "User Name: $name\n".
+  "User Email: $visitor_email\n".
 
-  "User Message: $message.\n";
+  "User Message: 
+  
+    $message\n";
 
   $to = "eduard.tupy@hotmail.com";
-  $headers = "From $email_from \r\n";
 
-
-  mail($to, $email_subject, $email_body, $headers);
+  mail($to, $email_subject, $email_body);
   header("Location: index.html");
 ?>
