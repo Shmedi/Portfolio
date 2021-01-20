c64ce1a250a76d46ebf0398e47cbccf8667bc161
Shmedi <eduard.tupy@hotmail.com>
2021-01-20 17:36:17 -0500

    Changed title animation and styling, added more items to skill section

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const shmedi = {};
 
+const textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 3250,
+    delay: (el, i) => 150 * (i+1)
+  });
+
 shmedi.navClick = () => {
   $(document).on("click", 'a[href^="#"]', function () {
     $("html, body")
