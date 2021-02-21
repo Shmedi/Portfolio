1bfb1c5ceb16ac80ce0da6fd740854c431a4bc8c
Shmedi <eduard.tupy@hotmail.com>
2021-02-20 23:02:32 -0500

    Added a loading page, added skill icon name on hover

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+$(window).on("load", () => {
+  $(".loaderWrapper").fadeOut("slow");
+});
+
 const shmedi = {};
 
-const textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+const textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
 
-anime.timeline({loop: false})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 1500,
-    delay: (el, i) => 150 * (i+1)
-  });
+anime.timeline({ loop: false }).add({
+  targets: ".ml3 .letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  duration: 1000,
+  delay: (el, i) => 150 * (i + 1),
+});
 
 shmedi.navClick = () => {
   $(document).on("click", 'a[href^="#"]', function () {
